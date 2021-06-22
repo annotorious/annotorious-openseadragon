31dba1e9917c065a41c547d361efbfa13be3c2ba
Rainer Simon <rainer.simon@ait.ac.at>
2021-06-22 11:39:55 +0200

    Reverting last change

--- src/OSDAnnotationLayer.js
+++ src/OSDAnnotationLayer.js
@@ -376,6 +376,8 @@ export default class OSDAnnotationLayer extends EventEmitter {
     const readOnly = this.readOnly || annotation.readOnly;
 
     if (!(readOnly || this.headless)) {
+      setTimeout(() => shape.parentNode.removeChild(shape), 1);
+
       const toolForAnnotation = this.tools.forAnnotation(annotation);
       this.selectedShape = toolForAnnotation.createEditableShape(annotation);
       this.selectedShape.scaleHandles(1 / this.currentScale());
@@ -403,8 +405,6 @@ export default class OSDAnnotationLayer extends EventEmitter {
 
       if (!skipEvent)
         this.emit('select', { annotation, element: this.selectedShape.element });
-
-      shape.parentNode.removeChild(shape);
     } else {
       this.selectedShape = shape;
 
