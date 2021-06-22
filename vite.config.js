2af702bc13729913c08a308ae311b0b0b1ec58a9
Rainer Simon <rainer.simon@ait.ac.at>
2021-06-22 15:59:39 +0200

    Closes #59

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import preact from '@preact/preset-vite';
+
+const path = require('path');
+
+export default defineConfig({
+  plugins: [ preact() ],
+  build: {
+    sourcemap: true,
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.jsx'),
+      name: 'OpenSeadragon.Annotorious',
+      formats: [ 'umd' ]
+    },
+    rollupOptions: {
+      external: 'openseadragon'
+    }
+  }
+})
