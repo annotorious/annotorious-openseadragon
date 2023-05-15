2e06d90f5c61de49909a38aac8fb14de4374f68c
rainyday321 <56607889+rainyday321@users.noreply.github.com>
2023-05-15 20:33:13 +0800

    [update] fix an issue where clicking canvas in 'rect' drawing mode will disable drawing

--- src/OSDAnnotationLayer.js
+++ src/OSDAnnotationLayer.js
@@ -200,7 +200,8 @@ export class AnnotationLayer extends EventEmitter {
       releaseHandler: evt => {
         if (this.tools.current.isDrawing) {
           firstDragDone = true;
-
+          // continue in dragging mode if moveHandler has not been fired
+          if (!started) return;
           const { x , y } = this.tools.current.getSVGPoint(evt.originalEvent);
           this.tools.current.onMouseUp(x, y, evt.originalEvent);
 
